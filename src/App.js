a319ef3a36963e8bd31f8340b99bf0719af0eaf8
josephecombs <joseph.e.combs@gmail.com>
2023-08-13 21:11:07 -0400

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import Footer from './components/Footer';
 import SimulatePage from './SimulatePage';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
           <Route path="/" element={<HomePage />} />
           <Route path="/simulate" element={<SimulatePage />} />
         </Routes>
+        <Footer />
       </Router>
     );
   }
